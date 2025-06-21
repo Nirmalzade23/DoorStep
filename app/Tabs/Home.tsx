1499e4e60ce9042b0f3776ce32da4a07aae1d2fb
Nirmalzade23 <nirmalzade24@gmail.com>
2025-06-21 18:43:39 +0530

    home page progress

--- app/Tabs/Home.tsx
+++ app/Tabs/Home.tsx
@@ -1,10 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { StyleSheet, TouchableOpacity, View, Text } from 'react-native'
+import ProfileImage from '../../component/ProfileImage'
+import { s , vs} from 'react-native-size-matters'
+import { router } from 'expo-router'
 
 const Home = () => {
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => router.push("/../component/Profile")}>
+          <ProfileImage />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.text}>
+        <Text style={{fontSize:s(40), fontWeight:"semibold"}}> What would you  </Text>
+        <Text style={{fontSize:s(40), fontWeight:"semibold", color:"#8C8D91"}}> like to order?</Text>
+      </View>
     </View>
   )
 }
@@ -12,9 +23,19 @@ const Home = () => {
 export default Home
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    backgroundColor:"#fff",
-  
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  header: {
+    flexDirection: "row",
+    right: s(-310),
+    paddingHorizontal: (-20),
+    marginTop: s(10)
+  },
+  text: { marginTop:vs(30),
+    justifyContent:"center",
+    alignItems:"center"
+
   }
 })
